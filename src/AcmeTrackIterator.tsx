46711424bc1e6183b3ae5086e91f6b9aafc4d468
Peter Styk <peter@styk.tv>
2020-01-06 21:12:12 +0000

    verify build remove old samples

--- src/AcmeTrackIterator.tsx
+++ src/AcmeTrackIterator.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
-import { useEffect, useState, useCallback, useReducer } from "react";
-import "./Track.css";
-import { Sampler } from "tone";
-import "./ToneSampler.css";
-import * as Tone from "tone";
+import { useEffect, useReducer, useState } from "react";
 import { Slider } from "react-nexusui";
+import * as Tone from "tone";
+import "./ToneSampler.css";
+import "./Track.css";
 
 //this is list of assets, temporary eventually will be rest call
 const assetList = {
@@ -59,29 +58,6 @@ const Track = props => {
   );
 };
 
-//just to complete example for now
-const initialState = { count: 0 };
-
-//reducer, based on example
-function reducer(state, action) {
-  switch (action.type) {
-    case "PLAY":
-      //this.sampler.triggerAttack("A1");
-      console.log("start clicked");
-
-      Tone.Transport.start("+0");
-      return;
-    case "STOP":
-      //this.sampler.triggerAttack("A1");
-      console.log("stop clicked");
-      Tone.Transport.stop();
-
-      return;
-    default:
-      throw new Error();
-  }
-}
-
 //this is track generator, each loop is a new track, one loop per track
 //we will try to put ToneJs transport here and have Track elements use that as global synchronizer
 const Tracks = props => {
@@ -96,12 +72,20 @@ const Tracks = props => {
   //these used to be in a class
   //i don't know how to put them inside of the const
   //basically we need to attach those to the buttons below
+  const onPlayClick = () => {
+      console.log("start clicked");
+      Tone.Transport.start("+0");
+  };
 
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const onStopClick = () => {
+    console.log("stop clicked");
+    Tone.Transport.stop();
+};
+
   return (
     <React.Fragment>
-      <button onClick={() => dispatch({ type: "PLAY" })}>PLAY</button>
-      <button onClick={() => dispatch({ type: "STOP" })}>STOP</button>
+      <button onClick={onPlayClick}>PLAY</button>
+      <button onClick={onStopClick}>STOP</button>
       {props.items.assets.map(item => (
         <React.Fragment key={item.guid}>
           <Track asset={item} />
